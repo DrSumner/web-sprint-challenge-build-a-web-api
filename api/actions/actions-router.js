586c3fe96eaa69a8ad391292df1f765e09cdffbd
DrSumner <157843168+DrSumner@users.noreply.github.com>
2024-07-08 17:34:58 -0400

    basic functionality

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,32 @@
 // Write your "actions" router here!
+const express= require('express')
+const actions = require('./actions-model')
+
+const router = express.Router();
+
+// GET ACTIONS
+router.get('/', (req,res) => {
+actions.get()
+.then( action => res.json(action))
+.catch(err => res.status(500).json(
+    {
+        message:'error getting actions',
+     err: err.message,
+    }
+))
+})
+
+// GET A ACTION
+router.get('/:id', (req,res) => {
+    const {id} = req.params
+    actions.get(id)
+    .then( action => res.json(action))
+    .catch(err => res.status(500).json(
+        {
+            message:'error getting actions',
+         err: err.message,
+        }
+    ))
+    })
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,34 @@
 // Write your "projects" router here!
+const express= require('express')
+const projects = require('./projects-model')
+
+const router = express.Router();
+
+// GET PROJECTS
+router.get('/', (req,res) => {
+    projects.get()
+.then( pros => res.json(pros))
+.catch(err => res.status(500).json(
+    {
+        message:'error getting projects',
+     err: err.message,
+     stack: err.stack,
+    }
+))
+})
+
+// GET  A PROJECT
+router.get('/:id', (req,res) => {
+    const {id} = req.params
+    projects.get(id)
+.then( pro => res.json(pro))
+.catch(err => res.status(500).json(
+    {
+        message:'error getting projects',
+     err: err.message,
+     stack: err.stack,
+    }
+))
+})
+
+module.exports = router;
